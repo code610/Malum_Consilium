2c11daff61a4fc8696270d2441c2e51571e7d29c
Austin A <achen@Austins-MacBook-Pro.local>
2021-09-10 16:26:18 -0700

    i

--- malum_consilium/stock_visualizer/views.py
+++ malum_consilium/stock_visualizer/views.py
@@ -10,21 +10,16 @@ import json
 
 
 APIKEY = 'WS7ZCU1NZV2PFEX7' 
-#replace 'my_alphav_api_key' with your actual Alpha Vantage API key obtained from https://www.alphavantage.co/support/#api-key
 
 
 DATABASE_ACCESS = True
-#if False, the app will always query the Alpha Vantage APIs regardless of whether the stock data for a given ticker is already in the local database
 
-
-#view function for rendering home.html
 def home(request):
     return render(request, 'home.html', {})
 
 @csrf_exempt
 def get_stock_data(request):
     if request.is_ajax():
-        #get ticker from the AJAX POST request
         ticker = request.POST.get('ticker', 'null')
         ticker = ticker.upper()
 
@@ -49,11 +44,9 @@ def get_stock_data(request):
         output_dictionary['sma'] = sma_series
         output_dictionary['macd'] = macd_series
         output_dictionary['bbands'] = bbands_series
-        #save the dictionary to database
         temp = StockData(symbol=ticker, data=json.dumps(output_dictionary))
         temp.save()
 
-        #return the data back to the frontend AJAX call 
         return HttpResponse(json.dumps(output_dictionary), content_type='application/json')
 
     else:
